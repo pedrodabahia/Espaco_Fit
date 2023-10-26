67d520da10cac12bd46cb0671fc20c2b4bbcff14
pedrodabahia <109925892+pedrodabahia@users.noreply.github.com>
2023-10-26 13:02:46 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -44,14 +44,14 @@ $(function(){
         $('header').css('margin','0').css(' margin-top','0').css('width','100%').css('height','100vh').css(' border-top-left-radius','0').css('border-top-right-radius','0').css('background-image','linear-gradient(to bottom right, rgb(0, 0, 0), rgb(0, 3, 54),rgb(0, 0, 0))');
         
         
-        $('section.sobre-empresa .text-sobre h1').css('font-size','10vw').css('text-shadow','0 1vw 3vw rgb(000,141,207)')
+        $('section.sobre-empresa .text-sobre h1').css('font-size','10vw');
         $('section.sobre-empresa .text-sobre p').css( 'font-size', '2vh').css( 'width','70%').css('margin',' 0').css('text-align','center').css('display','block');
         $('section.sobre-empresa .text-sobre p a').css('font-size','2vh');
-        $('section.sobre-empresa .text-sobre, section.sobre-empresa .banners-atletas').css('width','90%').css('height','45%').css('display','block;').css('overflow',' hidden').css('float','unset').css('display','inline-block').css('position','static').css('margin','9vw')..css('text-shadow',' 0 1vw 3vw rgb(000,141,207)');
-        $('section.sobre-empresa .text-sobre').css('padding', '5vh 0 0 0').css('height','auto').css('text-align','center').css('margin','2vw 5% 0 5%').css('text-shadow',' 0 1vw 3vw rgb(000,141,207)');
+        $('section.sobre-empresa .text-sobre, section.sobre-empresa .banners-atletas').css('width','90%').css('height','45%').css('display','block;').css('overflow',' hidden').css('float','unset').css('display','inline-block').css('position','static').css('margin','9vw')
+        $('section.sobre-empresa .text-sobre').css('padding', '5vh 0 0 0').css('height','auto').css('text-align','center').css('margin','2vw 5% 0 5%').css('text-shadow',' 0 1vw 3vw rgb(000,)');
         $('section.sobre-empresa .banners-atletas').css('align-items','center').css('text-align','center').css('overflow','none').css('margin','2vw 0 0 0 ');
        
-        $('section.sobre-empresa ').css('align-items','center').css('text-align','center').css('text-shadow',' 0 1vw 3vw rgb(000,141, 207)');;
+        $('section.sobre-empresa ').css('align-items','center').css('text-align','center');
        
         $('section.sobre-empresa .img-atleta:first-child').css('margin-left','9vw').css('box-shadow',' 0 1vw 3vw rgb(000, 141, 207);');
         $('section.sobre-empresa .img-atleta:last-child').css('margin-top','2vw').css('height','90%').css('box-shadow',' 0 1vw 3vw rgb(000, 141, 207);');;
