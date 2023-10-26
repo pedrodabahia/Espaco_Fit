a9174b29116f059afeeb6efec949d4fd7fa2887e
pedrodabahia <109925892+pedrodabahia@users.noreply.github.com>
2023-10-26 06:04:06 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -46,6 +46,7 @@ $(function(){
         $('section.sobre-empresa .text-sobre p').css( 'font-size', '2vh').css( 'width','70%').css('margin-left',' 9vw;').css(' text-align','justify').css('display','block');
         $('section.sobre-empresa .text-sobre p a').css('font-size','2vh').css('display',' block').css('float','left')
         $('section.sobre-empresa .text-sobre, section.sobre-empresa .banners-atletas').css('width','90%').css('height','45%').css('display','block;').css('overflow',' hidden').css('float','unset').css('display','inline-block').css('position','static').css('margin','0vw');
+        $('section.sobre-empresa .text-sobre').css('height', '50vw');
         $('section.sobre-empresa .banners-atletas').css('align-items','center').css('text-align','center').css('overflow','none');
         $('section.sobre-empresa ').css('align-items','center').css('text-align','center');
         $('section.sobre-empresa .img-atleta:first-child').css('margin-left','9vw');
@@ -54,8 +55,8 @@ $(function(){
         $('section.serviços .imagens .contentImgs .imagem h2').css('color','white');
         $('section.serviços .imagens .contentImgs .imagem:hover h2').css('text-shadow','0 0 20px rgb(000, 100, 220)');
         $('section.serviços .imagens').css('box-sizing','border-box').css('margin-top','-5vh');
-        $('section.serviços .title').css('margin-top','0').css('padding-top','40px').css('font-size','2.7vw').css(' height','40vw');
-        $('section.serviços .title h1').css('font-size','10vw');
+        $('section.serviços .title').css('margin-top','0').css('padding-top','14vw').css('font-size','4vw').css(' height','40vw');
+        $('section.serviços .title h1').css('font-size','13vw');
         $('section.serviços .imagens .contentImgs .textcomplement').css('display','flex');
         //$('section.depoimentos').css('','')
         $('section.formulario').css('width','100%').css('margin','2vw 0 0 0').css('border-radius','12px').css('border-bottom-left-radius','0').css('border-bottom-right-radius','0');
