263649098d8c95f5bc2b58b9c89ea8dd10471354
pedrodabahia <109925892+pedrodabahia@users.noreply.github.com>
2023-10-26 11:49:17 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -34,23 +34,28 @@ $(function(){
 
     function formatedSlider(){
         /*FUNCTION QUE FORMATA OS ESTILOS DA DI IMAGENS DE ACORDO COM O TAMANHOO DA TELA*/
+        $('*').css('outline','none')
         $('.serviços').attr('style','height: 95vh');
         $('.imagem').attr('style','width : 75vw;height:40vh; display : inline-block;margin-left:3vw;margin-top:6px;border-left: 3vw solid white;box-sizing:border-box');
         $('.contentImgs').attr('style','display:block; width : 279vw;height:60vh;');
         $('.imagens').attr('style',' overflow-y : hidden ; overflow-x : hidden; height:60vh;text-align:left;width:80vw; margin-left:-3vw;display:inline-block');
         $('body').css('background-color','white');
-        $('header,section.sobre-empresa, section.serviços,section.depoimentos, section.formulario').css('margin-top','2vw').css('border-radius','12px').css('background-image','linear-gradient(to bottom right, rgb(0, 0, 0), rgb(0, 3, 54),rgb(0, 0, 0))').css('width','100%').css('height','95vh').css('box-shadow','0 0 6px black');
+        $('header,section.sobre-empresa, section.serviços,section.depoimentos, section.formulario').css('margin-top','2vw').css('border-radius','12px').css('width','100%').css('height','95vh').css('box-shadow','0 0 6px black');
         $('header').css('margin','0').css(' margin-top','0').css('width','100%').css('height','100vh').css(' border-top-left-radius','0').css('border-top-right-radius','0')
-        $('section.sobre-empresa .text-sobre').css('padding', '5vh 0 0 0');
+        
+        
         $('section.sobre-empresa .text-sobre h1').css('font-size','10vw');
-        $('section.sobre-empresa .text-sobre p').css( 'font-size', '2vh').css( 'width','70%').css('margin-left',' 9vw;').css(' text-align','justify').css('display','block');
-        $('section.sobre-empresa .text-sobre p a').css('font-size','2vh').css('display',' block').css('float','left')
-        $('section.sobre-empresa .text-sobre, section.sobre-empresa .banners-atletas').css('width','90%').css('height','45%').css('display','block;').css('overflow',' hidden').css('float','unset').css('display','inline-block').css('position','static').css('margin','0vw');
-        $('section.sobre-empresa .text-sobre').css('height', '53vw');
-        $('section.sobre-empresa .banners-atletas').css('align-items','center').css('text-align','center').css('overflow','none');
+        $('section.sobre-empresa .text-sobre p').css( 'font-size', '2vh').css( 'width','70%').css('margin',' 0 vw;').css('text-align','center').css('display','block');
+        $('section.sobre-empresa .text-sobre p a').css('font-size','2vh')
+        $('section.sobre-empresa .text-sobre, section.sobre-empresa .banners-atletas').css('width','90%').css('height','45%').css('display','block;').css('overflow',' hidden').css('float','unset').css('display','inline-block').css('position','static').css('margin','9vw')
+        $('section.sobre-empresa .text-sobre').css('padding', '5vh 0 0 0').css('height','auto').css('text-align','center').css('margin','2vw auto 0 auto');
+        $('section.sobre-empresa .banners-atletas').css('align-items','center').css('text-align','center').css('overflow','none').css('margin','2vw 0 0 0 ');
+       
         $('section.sobre-empresa ').css('align-items','center').css('text-align','center');
-        $('section.sobre-empresa .img-atleta:first-child').css('margin-left','9vw');
-        $('section.sobre-empresa .img-atleta:last-child').css('margin-top','2vw').css('height','90%');
+       
+        $('section.sobre-empresa .img-atleta:first-child').css('margin-left','9vw').css('box-shadow',' 0 1vw 3vw rgb(000, 141, 207);');
+        $('section.sobre-empresa .img-atleta:last-child').css('margin-top','2vw').css('height','90%').css('box-shadow',' 0 1vw 3vw rgb(000, 141, 207);');;
+        
         $('section.serviços .imagens .contentImgs .imagem').css('text-align','center').css('border-shadow','0 1px 2px rgb(125, 125, 125)');
         $('section.serviços .imagens .contentImgs .imagem h2').css('color','white');
         $('section.serviços .imagens .contentImgs .imagem:hover h2').css('text-shadow','0 0 20px rgb(000, 100, 220)');
@@ -58,6 +63,7 @@ $(function(){
         $('section.serviços .title').css('margin-top','0').css('padding-top','14vw').css('font-size','4vw').css(' height','40vw');
         $('section.serviços .title h1').css('font-size','13vw');
         $('section.serviços .imagens .contentImgs .textcomplement').css('display','flex');
+       
         //$('section.depoimentos').css('','')
         $('section.formulario').css('width','100%').css('margin','2vw 0 0 0').css('border-radius','12px').css('border-bottom-left-radius','0').css('border-bottom-right-radius','0');
     /*    $('').css('','')
